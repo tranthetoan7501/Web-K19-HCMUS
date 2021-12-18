d85fd64e1e20d9fd71ee4394bf063bdbf2d8cfdd
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2021-12-19 06:14:16 +0700

    add ID model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
+    _id:{type:Schema.Types.ObjectId},
     name:{ type: String},
     username:{type:String},
     password:{type:String},
